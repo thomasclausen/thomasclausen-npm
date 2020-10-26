f6a62a971be209e7024ee89a9a396b64b5bf05fa
Thomas Clausen <kontakt@thomasclausen.dk>
2020-10-26 23:00:56 +0100

    Write a proper message

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+const chalk = require('chalk');
+
 module.exports = () => {
-	console.log(`Don't mind me...`);
+	console.log(chalk.cyan(`Hi, Thomas Clausen here...`));
+	console.log(chalk.cyan(`    Web: ` + chalk.bold(`thomasclausen.dk`)));
+	console.log(chalk.cyan(`Codepen: ` + chalk.bold(`codepen.io/thomasclausen`)));
+	console.log(chalk.cyan(` GitHub: ` + chalk.bold(`github.com/thomasclausen`)));
 };
