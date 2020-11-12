8b1b2cfb81724c2e3e8816347b3bfb79327ed05d
Thomas Clausen <kontakt@thomasclausen.dk>
2020-11-12 21:49:32 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const chalk = require('chalk');
 
 module.exports = () => {
+	console.log(chalk.cyan(``));
 	console.log(chalk.cyan(`Hi, Thomas Clausen here...`));
+	console.log(chalk.cyan(`I recommend you clean your cassette drive daily.`));
+	console.log(chalk.cyan(``));
 	console.log(chalk.cyan(`    Web: ` + chalk.bold(`thomasclausen.dk`)));
 	console.log(chalk.cyan(`Codepen: ` + chalk.bold(`codepen.io/thomasclausen`)));
 	console.log(chalk.cyan(` GitHub: ` + chalk.bold(`github.com/thomasclausen`)));
+	console.log(chalk.cyan(``));
 };
